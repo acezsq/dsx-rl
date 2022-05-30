30ef301221a8b8ae788798561d6abc3d7d6a03ea
zxs-000202 <2635718400@qq.com>
2022-05-30 18:05:41 +0800

    修补bug

--- eight.py
+++ eight.py
@@ -75,11 +75,11 @@ class DQN:
             max_next_q_values = self.target_q_net(next_states).gather(1, max_action)
         else:  # DQN的情况
             max_next_q_values = self.target_q_net(next_states).max(1)[0].view(-1, 1)
-        q_targets = rewards + self.gamma * max_next_q_values * (1 - dones)  # TD误差目标
-        dqn_loss = torch.mean(F.mse_loss(q_values, q_targets))  # 均方误差损失函数
-        self.optimizer.zero_grad()  # PyTorch中默认梯度会累积,这里需要显式将梯度置为0
-        dqn_loss.backward()  # 反向传播更新参数
-        self.optimizer.step()
+            q_targets = rewards + self.gamma * max_next_q_values * (1 - dones)  # TD误差目标
+            dqn_loss = torch.mean(F.mse_loss(q_values, q_targets))  # 均方误差损失函数
+            self.optimizer.zero_grad()  # PyTorch中默认梯度会累积,这里需要显式将梯度置为0
+            dqn_loss.backward()  # 反向传播更新参数
+            self.optimizer.step()
 
         if self.count % self.target_update == 0:
             self.target_q_net.load_state_dict(
